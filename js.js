e17565223222bd5f167e241e24e4efc556874f6f
Jacob Kiesel <jacobpkiesel@gmail.com>
2023-01-28 00:35:25 -0600

    First commit new portfolio

---
+++ js.js
@@ -0,0 +1,25 @@
+VANTA.DOTS({
+  el: "#net",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  showLines: false
+})
+
+// const effect = VANTA.DOTS({
+//   el: "#net",
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   scale: 1.00,
+//   scaleMobile: 1.00,
+//   showLines: false
+// }) 
+
+// effect.resize()
