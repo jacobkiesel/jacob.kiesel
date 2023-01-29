0e31465093269b4d26eaa371766448370e04240e
Jacob Kiesel <jacobpkiesel@gmail.com>
2023-01-28 22:09:05 -0600

    before npm start

--- js.js
+++ js.js
@@ -1,25 +1,14 @@
-VANTA.DOTS({
-  el: "#net",
+import * as THREE from "there";
+
+VANTA.TOPOLOGY({
+  el: "#your-element-selector",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  showLines: false
-})
-
-// const effect = VANTA.DOTS({
-//   el: "#net",
-//   mouseControls: true,
-//   touchControls: true,
-//   gyroControls: false,
-//   minHeight: 200.00,
-//   minWidth: 200.00,
-//   scale: 1.00,
-//   scaleMobile: 1.00,
-//   showLines: false
-// }) 
-
-// effect.resize()
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x16113c,
+  backgroundColor: 0x2b223e,
+});
